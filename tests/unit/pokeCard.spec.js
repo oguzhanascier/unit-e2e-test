145ebcfae774f95f81cca9b22792f74bb69eadb9
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-05 14:15:20 +0300

    test

--- tests/unit/pokeCard.spec.js
+++ tests/unit/pokeCard.spec.js
@@ -1,4 +1,21 @@
+import { expect } from "chai";
 import Vue from "vue";
 import pokeCard from "../../src/components/pokeCard";
 
-describe("pokeCard.vue", () => {});
+describe("pokeCard.vue", () => {
+  it("should list pokemon", () => {
+    const container=document.createElement('div')
+    const pokeCardComp = Vue.extend(pokeCard);
+    const vm= new pokeCardComp({
+        propsData:{
+            pokemon:[
+                {images: { '/assets/cms2/img/pokedex/full/': {url: '.png'}}},   
+                {images: { '/assets/cms2/img/pokedex/full/': {url: '.png'}}}
+            ]
+
+        }
+    })
+        vm.$mount(container);
+        expect(vm.pokemon.lenght).to.equal('')
+  });
+});
