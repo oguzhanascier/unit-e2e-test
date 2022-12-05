9f0327a0a508b9fa1be3fec39bf332218ac8c9c0
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-05 18:09:47 +0300

    beforeEach

--- tests/unit/pokeButton.spec.js
+++ tests/unit/pokeButton.spec.js
@@ -3,17 +3,27 @@ import Vue from "vue";
 import pokeButton from "../../src/components/pokeButton";
 
 describe("pokeButton.vue", () => {
+    let vm=null
+
+    beforeEach(()=>{
+        const container = document.createElement("div");
+        const pokeButtonComp = Vue.extend(pokeButton);
+         vm = new pokeButtonComp();
+        vm.$mount(container);
+    
+
+    })
+
   it("should be instantianed", () => {
-    const container = document.createElement("div");
-    const pokeButtonComp = Vue.extend(pokeButton);
-    const vm = new pokeButtonComp();
-    vm.$mount(container);
+ 
 
     /// firstChild & lastChild should not
 
     expect(vm.$el.firstChild.tagName).to.equal("BUTTON");
   });
 
+  it("should emit event when button is clicked", () => {
 
-  it('should emit event when button is clicked')
+
+  });
 });
