9aa226fc152540a9c03b2269d34241e2f05d276a
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-05 17:57:03 +0300

    test button

---
+++ tests/unit/pokeButton.spec.js
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+import Vue from "vue";
+import pokeButton from "../../src/components/pokeButton";
+
+describe("pokeCard.vue", () => {
+  it("should list pokemon", () => {});
+});
