83eea43e2adbbe2fd0529f228c91ec726b08e9b3
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-05 18:15:00 +0300

    sinon spy

--- tests/unit/pokeButton.spec.js
+++ tests/unit/pokeButton.spec.js
@@ -3,27 +3,24 @@ import Vue from "vue";
 import pokeButton from "../../src/components/pokeButton";
 
 describe("pokeButton.vue", () => {
-    let vm=null
+  let vm = null;
 
-    beforeEach(()=>{
-        const container = document.createElement("div");
-        const pokeButtonComp = Vue.extend(pokeButton);
-         vm = new pokeButtonComp();
-        vm.$mount(container);
-    
-
-    })
+  beforeEach(() => {
+    const container = document.createElement("div");
+    const pokeButtonComp = Vue.extend(pokeButton);
+    vm = new pokeButtonComp();
+    vm.$mount(container);
+  });
 
   it("should be instantianed", () => {
- 
-
-    /// firstChild & lastChild should not
+    //- firstChild & lastChild should not
 
     expect(vm.$el.firstChild.tagName).to.equal("BUTTON");
   });
 
   it("should emit event when button is clicked", () => {
-
-
+    const spy=sinon.spy(vm, '$emit')
+    vm.$el.firstChild.click()
+    expect(spy.called).to.be.true;
   });
 });
